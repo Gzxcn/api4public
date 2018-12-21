74b1f33bb8f22d1ccd32253743aad00ab04c5b4d
GongZx <23269087@qq.com>
2018-12-21 18:00:47 +0800

    添加快递单号，查询

--- app/controller/api.js
+++ app/controller/api.js
@@ -1,6 +1,6 @@
 // 'use strict';
 const axios = require("axios");
-
+const { URL } = require('url');
 const Controller = require('egg').Controller;
 class IndexController extends Controller {
   
@@ -26,6 +26,51 @@ class IndexController extends Controller {
     async latest() {
 
     }
+
+    async kuaidi(){
+        let url = "http://www.kuaidi100.com/query?";
+        let get_data = this.ctx.query;
+        let data = await axios.get(url + "type=" + get_data.type + "&postid=" + get_data.postid)
+        let d = data.data 
+        if (d.status == 200) {
+            console.log(d.data)
+            this.ctx.body = {
+                code : 200,
+                tip  : "成功",
+                data : d.data
+            }
+        } else {
+            this.ctx.body = {
+                code : 400 ,
+                tip  : d.message
+            }
+        }
+    }
+
+    async weather(){
+        let url = "https://www.tianqiapi.com/api/?version=v1&";
+        let get_data = this.ctx.query;
+        let k = Object.keys(get_data);
+        let query = [];
+        k.forEach( async (i,v)=>{
+            query.push(i + "=" + get_data[`${i}`])
+        })
+        if (query.length) {
+            query = query.join('&');
+            let r = await axios.get(url + query);
+            this.ctx.body = {
+                code : 200,
+                tip  : "成功",
+                data : r.data
+            }
+        } else {
+            this.ctx.body = {
+                code : 400 ,
+                tip  : "参数错误"
+            }
+        }
+    }
 }
+
 
 module.exports = IndexController;
--- app/router/api.js
+++ app/router/api.js
@@ -8,5 +8,11 @@ module.exports = app => {
 	const validate = app.middleware.validate;
 	router.get("/api/aim_lottery", validate(["expect"]), controller.api.aim_lottery)
 	router.get("/api/aim_lottery", controller.api.latest)
+	/**
+	 * type : 快递单号 ： 申通 shentong  等。 
+	 * postid ： 快递号
+	 */
+	router.get("/api/kuaidi",validate(["type","postid"]),controller.api.kuaidi)
 
+	router.get("/api/weather", validate(["?city", "?cityid"]), controller.api.weather)
 }
